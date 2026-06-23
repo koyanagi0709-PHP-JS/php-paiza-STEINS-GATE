3853b998ee2716d80f04f13a60f08eb5d2711342
koyanagi0709 <koyanagi0709@gmail.com>
2026-06-23 12:17:08 +0900

    正則の表現エントリーポイント２クリア

---
+++ stage_02.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * ルール
+ * １行目に半角スペース区切りで
+ * キャラクター数Nと１戦闘での獲得経験値Kが与えられる
+ * 続く２行目以降は、各キャラクターのレベルアップ経験値が与えられる
+ * 
+ * ゴール
+ * 全キャラクターがレベルアップするのに必要な戦闘回数を求め出力する
+*/
+/* 変数定義 */
+list($n,$k) = explode(' ',trim(fgets(STDIN)));
+// echo $n,$k;
+$members = array(); #各キャラクターの経験値を格納する配列
+$totalVal = 0;
+$count = 0; #必要戦闘回数
+
+/* 処理 */
+$k = doubleval($k);
+
+for($i=0;$i<$n;$i++) {
+    array_unshift($members,doubleval(trim(fgets(STDIN))));
+}
+$totalVal = array_sum($members);
+// echo $totalVal;
+
+
+do {
+    $totalVal -= $k;
+    $count++;
+}while($totalVal>0);
+
+echo $count;
